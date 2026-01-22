886bb8b81cb1f0623c8ae8b269f0f9ce09ca009d
taleem-darkulaib <taleem.darkulaib@gmail.com>
2026-01-22 13:54:30 +0300

    Auto-update public/scripts/firebase.js

--- public/scripts/firebase.js
+++ public/scripts/firebase.js
@@ -1,10 +1,10 @@
 firebase.initializeApp({
-	apiKey: "AIzaSyAvwm_WM9Zr8Fub4kUS7CaUscr9vGGr6FE",
-	authDomain: "taleem-plus.firebaseapp.com",
-	databaseURL: "https://taleem-plus-default-rtdb.firebaseio.com",
-	projectId: "taleem-plus",
-	storageBucket: "taleem-plus.appspot.com",
-	messagingSenderId: "675795218449",
-	appId: "1:675795218449:web:c32a0db03b0d33898501b0",
-	measurementId: "G-8M2SRGJZCN"
+	apiKey: "AIzaSyC0x5X7Ewuc7aZlZMKgGSkDHG09GaX3SLI",
+	authDomain: "taleem-demo.firebaseapp.com",
+	databaseURL: "https://taleem-demo-default-rtdb.firebaseio.com",
+	projectId: "taleem-demo",
+	storageBucket: "taleem-demo.firebasestorage.app",
+	messagingSenderId: "469345051017",
+	appId: "1:469345051017:web:c842d7e17745c38daa4158",
+	measurementId: "G-CWBVRR09FW"
 });
